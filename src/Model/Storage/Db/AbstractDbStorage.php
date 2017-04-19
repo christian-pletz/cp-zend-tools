c050aeb7916ff186e7c70ac3b84d1ca298de3686
c.pletz <c.pletz@wize.life>
2017-04-19 11:06:19 +0200

    Added Model\Storage\Db\AbstractDbStorage

---
+++ src/Model/Storage/Db/AbstractDbStorage.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @author      Christian Pletz <info@christian-pletz.de>
+ * @copyright   Copyright (c) 2017 Christian Pletz
+ */
+
+/**
+ * namespace definition and usage
+ */
+
+namespace Cp\Model\Storage\Db;
+
+use Zend\Db\ResultSet\HydratingResultSet;
+use Zend\Db\TableGateway\TableGatewayInterface;
+use Zend\Hydrator;
+
+/**
+ * Class AbstractDbStorage
+ * @package Cp\Model\Storage\Db
+ */
+abstract class AbstractDbStorage
+{
+    /**
+     * Table gateway
+     *
+     * @var TableGatewayInterface
+     */
+    private $tableGateway;
+
+    /**
+     * Hydrator
+     *
+     * @var Hydrator\ClassMethods
+     */
+    private $hydrator;
+
+    /**
+     * @return TableGatewayInterface
+     */
+    public function getTableGateway(): TableGatewayInterface
+    {
+        return $this->tableGateway;
+    }
+
+    /**
+     * @return Hydrator\ClassMethods
+     */
+    public function getHydrator(): Hydrator\ClassMethods
+    {
+        return $this->hydrator;
+    }
+
+    /**
+     * UserDbStorage constructor.
+     * @param TableGatewayInterface $tableGateway
+     */
+    public function __construct(TableGatewayInterface $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+
+        $resultSetPrototype = $this->tableGateway->getResultSetPrototype();
+
+        if ($resultSetPrototype instanceof HydratingResultSet) {
+            $this->hydrator = $resultSetPrototype->getHydrator();
+        }
+
+    }
+}
